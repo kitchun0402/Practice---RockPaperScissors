122034f5072a95b89188115d07c35476a84a1dd0
Kenneth Hau <hkc930402@gmail.com>
2021-07-07 16:28:13 +0800

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by kit chun on 7/7/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by kit chun on 7/7/2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
